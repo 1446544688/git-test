5c924e98854ba63d13c96fc85944665ce33c82a8
jxj <1446544688@qq.com>
2020-06-29 16:24:11 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/jxj/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.jxj.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+        System.out.printf("hello git project");
+    }
+}
